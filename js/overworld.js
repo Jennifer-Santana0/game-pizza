d69649bccc7c7f86cdf23155e65fec9239e11b9c
Jennifer Santana <jennifersouzadesantana@gmail.com>
2024-07-20 10:30:35 -0300

    Adicionando objetos ao jogo.

--- js/overworld.js
+++ js/overworld.js
@@ -1,3 +1,5 @@
+import { GameObject } from './GameObject.js'
+
 export class Overworld {
     constructor(config) {
         this.element = config.element
@@ -6,51 +8,26 @@ export class Overworld {
     }
 
     init() {
-        
         const image = new Image()
         image.onload = () => {
             this.ctx.drawImage(image, 0, 0)
         }
         image.src = './images/maps/DemoLower.png'
 
+        //Place some Game Objects
+        const hero = new GameObject({
+            x: 5,
+            y: 6
+        })
+        const npc1 = new GameObject({
+            x: 7,
+            y: 9,
+            src: './images/characters/people/npc1.png'
+        })
 
-        const x = 6
-        const y = 6
-
-        const shadow = new Image()
-        shadow.onload = () => {
-            this.ctx.drawImage(
-                shadow,
-                0,   //corte na horizontal
-                0,   //corte na vertical
-                32,   //quanto ele vai cortar na horizontal
-                32,   //quanto ele vai cortar na vertical
-                x * 16 - 8,    //posicao da imagem na horizontal
-                y * 16 - 18,    //posicao da imagem na vertical
-                32,   //largura
-                32    //altura
-            )
-        }
-        shadow.src = './images/characters/shadow.png'
-
-        const hero = new Image()
-        hero.onload = () => {
-            this.ctx.drawImage(
-                hero,
-                0,   //corte na horizontal
-                0,   //corte na vertical
-                32,   //quanto ele vai cortar na horizontal
-                32,   //quanto ele vai cortar na vertical
-                x * 16 - 8,    //posicao da imagem na horizontal
-                y * 16 - 18,    //posicao da imagem na vertical
-                32,   //largura
-                32    //altura
-            )
-        }
-        hero.src = './images/characters/people/hero.png'
-
+        setTimeout(() => {
+            hero.sprite.draw(this.ctx)
+            npc1.sprite.draw(this.ctx)
+        }, 200)
     }
-
-    
 }
-
